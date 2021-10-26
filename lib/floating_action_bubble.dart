33a6bd0ced656fb4882243a5a55825ea11fbd215
Mohammad Hammadi <hammadimohammad@gmail.com>
2021-10-26 13:55:56 +0300

    Add mini property(bool) to Floating action button

--- lib/floating_action_bubble.dart
+++ lib/floating_action_bubble.dart
@@ -1,48 +1,49 @@
 import 'package:flutter/material.dart';
 
 class Bubble {
-  const Bubble({
-    required this.title,
-    required this.onPress,
-    this.titleStyle,
-    this.iconColor,
-    this.bubbleColor,
-    this.icon,
-  });
+  const Bubble(
+      {@required this.title,
+      @required this.titleStyle,
+      @required this.iconColor,
+      @required this.bubbleColor,
+      @required this.icon,
+      @required this.onPress});
 
-  final IconData? icon;
-  final Color? iconColor;
-  final Color? bubbleColor;
-  final VoidCallback onPress;
+  final IconData icon;
+  final Color iconColor;
+  final Color bubbleColor;
+  final Function onPress;
   final String title;
-  final TextStyle? titleStyle;
+  final TextStyle titleStyle;
 }
 
 class BubbleMenu extends StatelessWidget {
-  const BubbleMenu(this.item, {Key? key}) : super(key: key);
+  const BubbleMenu(this.item, {Key key}) : super(key: key);
 
   final Bubble item;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-          shape: const StadiumBorder(),
-          padding:
-          const EdgeInsets.only(top: 11, bottom: 13, left: 32, right: 32),
-          elevation: 2,
-          primary: item.bubbleColor),
+    return MaterialButton(
+      shape: StadiumBorder(),
+      padding: EdgeInsets.only(top: 11, bottom: 13, left: 32, right: 32),
+      color: item.bubbleColor,
+      splashColor: Colors.grey.withOpacity(0.1),
+      highlightColor: Colors.grey.withOpacity(0.1),
+      elevation: 2,
+      highlightElevation: 2,
+      disabledColor: item.bubbleColor,
       onPressed: item.onPress,
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           item.icon != null
               ? Icon(
-            item.icon,
-            color: item.iconColor,
-          )
+                  item.icon,
+                  color: item.iconColor,
+                )
               : Container(),
-          const SizedBox(
+          SizedBox(
             width: 10.0,
           ),
           Text(
@@ -57,41 +58,43 @@ class BubbleMenu extends StatelessWidget {
 
 class _DefaultHeroTag {
   const _DefaultHeroTag();
-
   @override
   String toString() => '<default FloatingActionBubble tag>';
 }
 
 class FloatingActionBubble extends AnimatedWidget {
   const FloatingActionBubble({
-    required this.items,
-    required this.onPress,
-    required Animation animation,
-    this.iconColor,
-    this.backGroundColor,
+    @required this.items,
+    @required this.onPress,
+    @required this.iconColor,
+    @required this.backGroundColor,
+    @required Animation animation,
     this.herotag,
     this.iconData,
     this.animatedIconData,
+	this.mini,
   })  : assert((iconData == null && animatedIconData != null) ||
-      (iconData != null && animatedIconData == null)),
+            (iconData != null && animatedIconData == null)),
         super(listenable: animation);
 
   final List<Bubble> items;
-  final VoidCallback onPress;
-  final AnimatedIconData? animatedIconData;
-  final Object? herotag;
-  final IconData? iconData;
-  final Color? iconColor;
-  final Color? backGroundColor;
+  final Function onPress;
+  final AnimatedIconData animatedIconData;
+  final Object herotag;
+  final IconData iconData;
+  final Color iconColor;
+  final Color backGroundColor;
+  final bool? mini;
 
   get _animation => listenable;
 
   Widget buildItem(BuildContext context, int index) {
     final screenWidth = MediaQuery.of(context).size.width;
 
-    final textDirection = Directionality.of(context);
+    TextDirection textDirection =
+        Directionality.of(context) ?? TextDirection.ltr;
 
-    final animationDirection = textDirection == TextDirection.ltr ? -1 : 1;
+    double animationDirection = textDirection == TextDirection.ltr ? -1 : 1;
 
     final transform = Matrix4.translationValues(
       animationDirection *
@@ -125,28 +128,30 @@ class FloatingActionBubble extends AnimatedWidget {
           ignoring: _animation.value == 0,
           child: ListView.separated(
             shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            separatorBuilder: (_, __) => const SizedBox(height: 12.0),
-            padding: const EdgeInsets.symmetric(vertical: 12),
+            physics: NeverScrollableScrollPhysics(),
+            separatorBuilder: (_, __) => SizedBox(height: 12.0),
+            padding: EdgeInsets.symmetric(vertical: 12),
             itemCount: items.length,
             itemBuilder: buildItem,
           ),
         ),
         FloatingActionButton(
-          heroTag: herotag ?? const _DefaultHeroTag(),
+          heroTag: herotag == null ? const _DefaultHeroTag() : herotag,
           backgroundColor: backGroundColor,
           // iconData is mutually exclusive with animatedIconData
           // only 1 can be null at the time
-          child: iconData == null && animatedIconData != null
+          child: iconData == null
               ? AnimatedIcon(
-            icon: animatedIconData!,
-            progress: _animation,
-          )
+                  icon: animatedIconData,
+                  progress: _animation,
+                  color: iconColor,
+                )
               : Icon(
-            iconData,
-            color: iconColor,
-          ),
+                  iconData,
+                  color: iconColor,
+                ),
           onPressed: onPress,
+		  mini: mini ?? false,
         ),
       ],
     );
